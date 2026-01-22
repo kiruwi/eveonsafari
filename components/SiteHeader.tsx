ceaddfd3eb404ee4fa281f88d9bba33caccff915
kiruwi <i4nkirui@gmail.com>
2026-01-22 16:27:43 +0300

    national Parks Link

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -92,8 +92,8 @@ const NAV_ITEMS: NavItem[] = [
     links: [
       {
         label: "National Parks",
-        href: "/discover-tanzania/national-parks",
-        ariaLabel: "Explore Tanzania national parks",
+        href: "/itineraries#safari-grid",
+        ariaLabel: "Browse safari packages",
       },
       {
         label: "Islands",
