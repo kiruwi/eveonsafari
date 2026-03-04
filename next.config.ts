4d045fd19ed354715606521c835dff45575b018b
kiruwi <i4nkirui@gmail.com>
2026-03-04 19:56:45 +0300

    update

--- next.config.ts
+++ next.config.ts
@@ -71,6 +71,56 @@ const nextConfig: NextConfig = {
         destination: "/zanzibar/5-days/zanzibar-5-days",
         permanent: true,
       },
+      {
+        source: "/serengeti-national-park",
+        destination: "/discover-tanzania/national-parks/serengeti-national-park",
+        permanent: true,
+      },
+      {
+        source: "/lake-eyasi",
+        destination: "/activities/cultural-visits",
+        permanent: true,
+      },
+      {
+        source: "/safari_package/10-day-best-of-tanzania-safari-wildlife-lakes-culture",
+        destination: "/safaris/10-day-best-of-tanzania",
+        permanent: true,
+      },
+      {
+        source: "/safari_package/8-day-tanzania-safari-migration-cultural-wonders",
+        destination: "/safaris/8-day-migration-cultural-wonders",
+        permanent: true,
+      },
+      {
+        source: "/ngorongoro-crater-day-trip",
+        destination: "/activities/day-trips/ngorongoro-crater-day-trip",
+        permanent: true,
+      },
+      {
+        source: "/zanzibar_tours/2-days-zanzibar-highlights-getaway",
+        destination: "/zanzibar/5-days/zanzibar-5-days",
+        permanent: true,
+      },
+      {
+        source: "/rubondo-island-national-park-tanzania-the-ultimate-travel-guide",
+        destination: "/discover-tanzania/national-parks",
+        permanent: true,
+      },
+      {
+        source: "/days-tanzania-safari-ngorongoro-crater-adventure",
+        destination: "/safaris/2-day-ngorongoro-crater-adventure",
+        permanent: true,
+      },
+      {
+        source: "/safari_package/2-day-tanzania-safari-ngorongoro-crater-adventure",
+        destination: "/safaris/2-day-ngorongoro-crater-adventure",
+        permanent: true,
+      },
+      {
+        source: "/hiking-and-trekking",
+        destination: "/trekking",
+        permanent: true,
+      },
     ];
   },
   images: {
